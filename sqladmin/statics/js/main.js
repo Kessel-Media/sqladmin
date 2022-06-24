7cd9ba5dd7fe981ba6e125780f3b3e4ae495277f
thibault <thibaultlebrun@live.fr>
2022-06-24 19:04:25 +0200

    fix delete modal

--- sqladmin/statics/js/main.js
+++ sqladmin/statics/js/main.js
@@ -1,13 +1,17 @@
 // Handle delete modal
-$(document).on('shown.bs.modal', '#modal-delete', function (event) {
-  var element = $(event.relatedTarget);
+const myModal = document.getElementById('modal-delete')
 
-  var name = element.data("name");
-  var pk = element.data("pk");
-  $("#modal-delete-text").text("This will permanently delete " + name + " " + pk + " ?");
+if (myModal) {
+  myModal.addEventListener('shown.bs.modal', function (event) {
+    var element = $(event.relatedTarget);
 
-  $("#modal-delete-button").attr("data-url", element.data("url"));
-});
+    var name = element.data("name");
+    var pk = element.data("pk");
+    // $("#modal-delete-text").text("This will permanently delete " + name + " " + pk + " ?");
+
+    $("#modal-delete-button").attr("data-url", element.data("url"));
+  });
+}
 
 $(document).on('click','#modal-delete-button',function() {
   $.ajax({
